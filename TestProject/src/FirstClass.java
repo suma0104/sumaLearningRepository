219cec338576e2dadeab0837b4636478577312af
suma K S <sumakiran.au@gmail.com>
2017-02-28 20:07:16 +1100

    first change

--- TestProject/src/FirstClass.java
+++ TestProject/src/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Suma");
+		System.out.println("Bye Suma");
 	}
 
 }
